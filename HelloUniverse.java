1eb16cf50f22173621fff9658ae35528274490f1
stevej <jatsasteve@gmail.com>
2024-07-29 06:30:37 +0200

    premier classe Planete

--- HelloUniverse.java
+++ HelloUniverse.java
@@ -1,13 +1,28 @@
 public class HelloUniverse {
     public static void main(String[] args) {
-        String str = "En %d, les planetes du systme solaire etaient au nombre de: %d ";
-        int nbPlanet = 0;
-        short annee = 2004;
-        if (annee < 2006) {
-            nbPlanet = 9;
-        } else {
-            nbPlanet = 8;
-        }
-        System.out.printf(str, annee, nbPlanet);
+        Planete planete = new Planete();
+        Planete mercure = new Planete();
+        Planete venus = new Planete();
+        Planete terre = new Planete();
+        Planete mars = new Planete();
+
+        mercure.nom = "Mercure";
+        mercure.matiere = "tellurique";
+        mercure.diametre = 4880;
+
+        venus.nom = "Venus";
+        venus.matiere = mercure.matiere;
+        venus.diametre = 12100;
+
+        terre.nom = "Terre";
+        terre.matiere = mercure.matiere;
+        terre.diametre = 12756;
+
+        mars.nom = "Mars";
+        mars.matiere = mercure.matiere;
+        mars.diametre = 6792;
+
+        System.out.println(mars.nom + " est une planete " + mars.matiere + " avec un diametre de " + mars.diametre + " kilometres");
+        System.out.println(planete.nom + " est une planete " + planete.matiere + " avec un diametre de " + planete.diametre + " kilometres");
     }
 }
---
+++ Planete.java
@@ -0,0 +1,5 @@
+public class Planete {
+    String nom;
+    String matiere;
+    long diametre;
+}
